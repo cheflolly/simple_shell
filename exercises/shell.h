1fe1e80fe737b9ca0d5689fef52ba2d3667625f9
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-11-07 19:54:28 +0100

    exercises

---
+++ exercises/shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int add(int a, int b);
+
+#endif /* SHELL_H */
